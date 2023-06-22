fc20de92fd4d544a1d69fd5037c8c7228191227a
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-22 18:27:34 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -334,28 +334,8 @@ public class MainActivity extends Activity {
     }
 
     private void checkForSpecificDiv() {
-//        Toast.makeText(MainActivity.this, "checkForSpecificDiv called", Toast.LENGTH_SHORT).show();
-String positionScript = "function getElementScreenPosition(el) {" +
-    "  var rect = el.getBoundingClientRect();" +
-    "  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;" +
-    "  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;" +
-    "  return {" +
-    "    x: rect.left + scrollLeft," +
-    "    y: rect.top + scrollTop" +
-    "  };" +
-    "}" +
-    "var adBoosters = document.querySelectorAll('#adBooster');" +
-    "for (var i = 0; i < adBoosters.length; i++) {" +
-    "  if (isElementVisible(adBoosters[i])) {" +
-    "    var position = getElementScreenPosition(adBoosters[i]);" +
-    "    console.log('Element with ID \"adBooster\" is visible');" +
-    "    console.log('Position (x, y):', position.x, position.y);" +
-    "    // Perform any desired actions with the x and y coordinates" +
-    "  }" +
-    "}";
-
-        webView.evaluateJavascript("javascript:(function () { " +
-                        "function getElementScreenPosition(el) {" +
+        Toast.makeText(MainActivity.this, "checkForSpecificDiv called", Toast.LENGTH_SHORT).show();
+        webView.evaluateJavascript("javascript: function getElementScreenPosition(el) {" +
                         "  var rect = el.getBoundingClientRect();" +
                         "  var scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;" +
                         "  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;" +
@@ -372,8 +352,7 @@ String positionScript = "function getElementScreenPosition(el) {" +
                         "  } else {" +
                         "    window. AndroidInterface.onSpecificDivVisible(false, 0, 0);" +
                         "  }" +
-                        "};" +
-                        "})()",
+                        "};()",
                 null
         );
     }
